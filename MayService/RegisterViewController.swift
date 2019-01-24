992c7e8885855701b0e147b321e465f3a7569da0
Supasit.s <asd@com.com>
2019-01-24 15:36:15 +0700

    Check Space

--- MayService/RegisterViewController.swift
+++ MayService/RegisterViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class RegisterViewController: UIViewController {
 
+    //Explicit
+    var name: String?
+    var user: String?
+    var pass: String?
     
     
     @IBOutlet weak var nameTextField: UITextField!
@@ -27,6 +31,25 @@ class RegisterViewController: UIViewController {
     
     
     @IBAction func uploadButton(_ sender: UIBarButtonItem) {
-    }
+        
+        //Get value from TextField
+        name = nameTextField.text
+        user = userTextField.text
+        pass = passTextField.text
+        
+        //Show Log
+        print("name ==> \(String(describing: name))")
+        print("user ==> \(String(describing: user))")
+        print("pass ==> \(String(describing: pass))")
+        
+        //Check Space
+        if (name!.count == 0) || (user!.count == 0) || (pass!.count == 0) {
+            //Have Space
+            print("Have Space!!!")
+        } else{
+            print("No Space")
+        }
+        
+    } //Upload Button
     
 } //Main Class

91423ac68848372c5082593b1da256a2b79ab00c
Supasit.s <asd@com.com>
2019-01-24 11:46:21 +0700

    Initial Commit

---
+++ MayService/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MayService
+//
+//  Created by TST-APP-01 on 24/1/2562 BE.
+//  Copyright © 2562 Teerawut. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

71498201d531b4579098a0d69ce7844a8f629e8e
Supasit.s <asd@com.com>
2019-01-25 16:03:24 +0700

    Map Feature

--- MayService/ServiceViewController.swift
+++ MayService/ServiceViewController.swift
@@ -1,19 +1,65 @@
-//
-//  ServiceViewController.swift
-//  MayService
-//
-//  Created by TST-APP-01 on 25/1/2562 BE.
-//  Copyright © 2562 Teerawut. All rights reserved.
-//
-
 import UIKit
+import MapKit
 
 class ServiceViewController: UIViewController {
+    
+    
+    @IBOutlet weak var myMapView: MKMapView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        showMapType2(lat: 13.606352, long:100.765416)
+        //showMap(lat: 13.606352, long:100.765416)
+    } //Main Method
+    
+    func showMapType2(lat: Double, long: Double) -> Void {
+        
+        let latitude:CLLocationDegrees = lat
+        let longtitude:CLLocationDegrees = long
+        
+        let laDelta:CLLocationDegrees = 0.01
+        let longDelta:CLLocationDegrees = 0.01
+        
+        let span: MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: laDelta, longitudeDelta: longDelta)
+        let Office:CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: latitude, longitude: longtitude)
+        let region:MKCoordinateRegion = MKCoordinateRegion(center: Office, span: span)
+        
+        
+        self.myMapView.setRegion(region, animated: true)
+        
+        //Office Anotation
+        let OfficeAnotation = MKPointAnnotation()
+        OfficeAnotation.coordinate = Office
+        OfficeAnotation.title = "HITACHI TRANSPORT SYSTEM VANTEC"
+        OfficeAnotation.subtitle = "Head Office (BLC)"
+        self.myMapView.addAnnotation(OfficeAnotation)
+        
+        //Bus Stop
+        
+        let latBranch:CLLocationDegrees = 13.573096
+        let longtBranch:CLLocationDegrees = 100.765745
+        let BranchStopAnotation = MKPointAnnotation()
+        BranchStopAnotation.coordinate = CLLocationCoordinate2D(latitude: latBranch, longitude: longtBranch)
+        BranchStopAnotation.title = "HITACHI TRANSPORT SYSTEM VANTEC"
+        BranchStopAnotation.subtitle = "(BLC5)"
+        self.myMapView.addAnnotation(BranchStopAnotation)
+        
+    }
+    
+    
+    func showMap(lat: Double,long: Double) -> Void {
+        
+        let homeCoordinate = CLLocationCoordinate2DMake(lat, long)
+        let regionDistance: CLLocationDistance = 1000
+        let regionSpan = MKCoordinateRegion.init(center: homeCoordinate, latitudinalMeters: regionDistance, longitudinalMeters: regionDistance)
+        let options = [MKLaunchOptionsMapCenterKey: NSValue(mkCoordinate: regionSpan.center), MKLaunchOptionsMapSpanKey: NSValue(mkCoordinateSpan: regionSpan.span)]
+        
+        let placeMark = MKPlacemark(coordinate: homeCoordinate)
+        let mapItem = MKMapItem(placemark: placeMark)
+        mapItem.name = "Home"
+        mapItem.openInMaps(launchOptions: options)
     }
 
-}
+} // Main Class

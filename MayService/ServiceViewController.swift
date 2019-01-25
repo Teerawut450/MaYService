da51dee367b3909b032e2155ae05b1fdd444462a
Supasit.s <asd@com.com>
2019-01-25 11:10:55 +0700

    Outlet and Action at Main Scene

---
+++ MayService/ServiceViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ServiceViewController.swift
+//  MayService
+//
+//  Created by TST-APP-01 on 25/1/2562 BE.
+//  Copyright © 2562 Teerawut. All rights reserved.
+//
+
+import UIKit
+
+class ServiceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
--- MayService/ViewController.swift
+++ MayService/ViewController.swift
@@ -10,11 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var userTextField: UITextField!
+    @IBOutlet weak var passTextField: UITextField!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    } //Main Method
+    
+    @IBAction func loginButton(_ sender: UIButton) {
+        
     }
-
-
 }
 
